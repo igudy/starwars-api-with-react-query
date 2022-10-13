7ba041f66427602e9bfadde784734083db554a5a
igudy <goodnessigunma1@gmail.com>
2022-10-13 10:43:47 +0100

    Pagination added - All features completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Navbar from "./components/Navbar";
+import People from "./components/People";
+import Planets from "./components/Planets";
 
 function App() {
+  const [page, setPage] = useState("planets");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="App">
+          <h1>Star wars info</h1>
+          <Navbar setPage={setPage} />
+          <div className="content">
+            {page === "planets" ? <Planets /> : <People />}
+          </div>
+        </div>
+        <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
